a1545d8713f2ed497da96182d2835684c4611d85
Friscxion <erwann.champion@outlook.fr>
2022-05-17 11:16:53 +0200

    mms

--- index.js
+++ index.js
@@ -4,6 +4,30 @@ const CHOIX = {
     ciseau:3,
 }
 
-function whoWin(blue,red){
-
+function isWin(player,opponent){
+    if (!Object.keys(CHOIX).includes(player) || !Object.keys(CHOIX).includes(opponent)) {
+        return undefined;
+    }
+    if(CHOIX[player]===CHOIX[opponent]) return null;
+    switch(CHOIX[player]){
+        case CHOIX.pierre:
+            return CHOIX[opponent]===CHOIX.feuille?false:true;
+        case CHOIX.feuille:
+            return CHOIX[opponent]===CHOIX.ciseau?false:true;
+        case CHOIX.ciseau:
+            return CHOIX[opponent]===CHOIX.pierre?false:true;
+    }
 }
+
+
+function main(player,opponent){
+    let playerWin=isWin(player,opponent);
+    if(playerWin===undefined) console.log("Un des input n'est pas valable!");
+    else if(playerWin===null)console.log("Egalité!");
+    else if(playerWin)console.log("Player a gagné!");
+    else console.log("Player a perdu");
+}
+
+main("pierre","pierre");
+
+module.exports={isWin}
---
+++ index.test.js
@@ -0,0 +1,22 @@
+const {isWin}=require('./index');
+
+
+test('Player win', () => {
+    expect(isWin("ciseau","feuille")).toBe(true);
+});
+test('Player loose', () => {
+    expect(isWin("pierre","feuille")).toBe(false);
+});
+
+test('Egalité', () => {
+    expect(isWin("pierre","pierre")).toBe(null);
+});
+
+
+test('Mauvaise entrée', () => {
+    expect(isWin("pier","pierre")).toBe(undefined);
+});
+test('Mauvaise 2', () => {
+    expect(isWin("pier")).toBe(undefined);
+});
+
